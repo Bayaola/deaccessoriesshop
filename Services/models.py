2f59c8e8bcf3fa26963b6dd34c02dfa76719af81
BilaLamouYannick <yannickbl93@gmail.com>
2022-04-04 12:01:27 +0100

    heruko

--- Services/models.py
+++ Services/models.py
@@ -1,5 +1,6 @@
 from distutils import text_file
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
@@ -8,6 +9,12 @@ class Service(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     desc = models.CharField(max_length=2500, blank=True)
     content = models.TextField()
+    image = models.ImageField(
+        help_text=_("Upload a product image"),
+        upload_to="images/",
+        default="images/default.png",
+        blank=True
+    )
     
     def __str__(self):
         return self.title
