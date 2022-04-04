136231de47793e4cee20e0f9f1f751367d050468
BilaLamouYannick <yannickbl93@gmail.com>
2022-04-04 13:29:05 +0100

    connect to elephantsql 2

--- src/settings.py
+++ src/settings.py
@@ -128,7 +128,6 @@ else:
         }
     }
 
-postgres://devsdcak:rMcEBoai0vTEKTuSlVW7HnwOQemeok4-@hattie.db.elephantsql.com/devsdcak
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
