f7510581a3f591b5b14dd1a1bcac89f60440410c
BilaLamouYannick <yannickbl93@gmail.com>
2022-06-10 13:52:37 +0100

    whatsapp message 2

--- src/settings.py
+++ src/settings.py
@@ -29,9 +29,9 @@ UPLOADCARE = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['designgenius.herokuapp.com', '127.0.0.1', 'designgenius.biz', '*']
+ALLOWED_HOSTS = ['127.0.0.1', 'deaccessoriesshop.com', '*']
 
 
 # Application definition
